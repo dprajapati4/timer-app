073702d4fb18233c84ecf182f41806092d087577
Deep <dprajap2@gmail.com>
2024-06-13 17:07:59 -0400

    add basic outline and css

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './Counter.module.css';
+import styles from "./Counter.module.css";
 
 export function Counter() {
-
   return (
-    <div>
-     
-
+    <div className={styles.counter}>
+      <div className={styles.input}>
+        <label for="task">Task</label>
+        <input type="text" />
+        <label for="time">Time:</label>
+        <input type="number" min={0} />
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+      <div className="table">
+        <table>
+          <tr>
+            <th>Task</th>
+            <th>Time Required</th>
+          </tr>
+          <tr>
+            <td>Test 1</td>
+            <td>4</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
